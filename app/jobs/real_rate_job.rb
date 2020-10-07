5a3c7da57006eee8c6dc56b65eac035779b35a2a
Dmitrii Neverov <dmi.neverov@gmail.com>
2020-10-07 17:07:20 +1000

    scrap rate from site

--- app/jobs/real_rate_job.rb
+++ app/jobs/real_rate_job.rb
@@ -1,5 +1,6 @@
 class RealRateJob < ApplicationJob
   queue_as :default
+  retry_on DataServices::RealRate::RateSiteError, wait: 10.minutes, attempts: 100
 
   def perform
     DataServices::RealRate.create
--- app/services/data_services/real_rate.rb
+++ app/services/data_services/real_rate.rb
@@ -1,5 +1,20 @@
 class DataServices::RealRate
+  class RateSiteError < StandardError; end
+
+  SOURCE_URL = 'https://www.cbr-xml-daily.ru/daily.xml'
+  CHAR_CODE = 'USD'
+
   def self.create
-    Rate.create price: rand(0...99.99).round(2)
+    response = Net::HTTP.get_response(URI(SOURCE_URL))
+    raise RateSiteError unless response.is_a?(Net::HTTPSuccess) && response.code == '200'
+
+    begin
+      price = Hash.from_xml(response.body)['ValCurs']['Valute']
+                  .find { |valute| valute['1CharCode'] == CHAR_CODE }['Value']
+                  .tr(',', '.').to_f
+    rescue
+      raise RateSiteError
+    end
+    Rate.create price: price
   end
 end
